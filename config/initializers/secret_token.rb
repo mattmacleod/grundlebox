45d335f6d29bf816165aa3c5a4e633b7db55ed92
Matthew MacLeod <matt@matt-m.co.uk>
2010-12-06 13:58:21 +0000

    Initial commit of a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grundlebox::Application.config.secret_token = '71cea82cd22eb3888f790a213b95a56d5f3c7bcb8055ec83db6cda0f72f1de174793f72df4965eb1741363205ca5e390284a0be14d60370b9d39622a8ccca406'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Grundlebox::Application.config.session_store :cookie_store, :key => '_grundlebox_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Grundlebox::Application.config.session_store :active_record_store
