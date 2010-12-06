7b4bd1fc2287fd02f698a13c16714ab855747c8f
Matthew MacLeod <matt@matt-m.co.uk>
2010-12-06 14:41:47 +0000

    Added some test helpers and installed gems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,48 @@
+# Setup the test environment
+##############################################################################
 ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+
+
+# Load test helper
+##############################################################################
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+# Load Shoulda macros
+##############################################################################
+Dir[File.expand_path(File.dirname(__FILE__) + '/shoulda_macros/*.rb')].each {|file| require file }
+
+
+# Fix weird routing issue
+##############################################################################
+Grundlebox::Application.routes.inspect
+
+class ActiveSupport::TestCase
+  fixtures :all
+  
+  
+  # Add some general test helpers
+  ############################################################################
+  
+  # Check for elements being included in collections
+  def assert_includes(elem, array, message = nil)
+    message = build_message message, '<?> is not found in <?>.', elem, array
+    assert_block message do
+      array.include? elem
+    end
+  end
+  
+  # Helper to massage XML into HTML so we can use test helpers on it
+  def xml_document
+    @xml_document ||= HTML::Document.new(@response.body, false, true)
+  end
+  
+  # An addon to the assert_select helper to do the same to XML (using the
+  # method mentioned above)
+  def assert_xml_select(*args, &block)
+    @html_document = xml_document
+    assert_select(*args, &block)
+  end
+  
 end
