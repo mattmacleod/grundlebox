265a66fc09a3d2010ddb4ee3e944192575ade0cf
Matthew MacLeod <matt@matt-m.co.uk>
2011-02-27 20:17:57 +0000

    Updated packaging tools to use packaged versions when running as Engine

--- app/helpers/grundlebox/application_helper.rb
+++ app/helpers/grundlebox/application_helper.rb
@@ -20,4 +20,40 @@ module Grundlebox::ApplicationHelper
     return output.html_safe
   end
   
+  def include_assets( group = :grundlebox_admin )
+    out = ""
+    
+    if group == :grundlebox_admin
+    	if defined?( Grundlebox::Application )
+  		  out << include_stylesheets(group, :media => "all")
+  		  out << include_javascripts(group)
+  		else
+  			out << '<!--[if (!IE)|(gte IE 8)]><!-->'
+  			out << '<link href="/packages/' + group.to_s + '-datauri.css?' + Grundlebox::Version + '" media="all" rel="stylesheet" type="text/css" />'
+  			out << '<!--<![endif]-->'
+  			out << '<!--[if lte IE 7]>'
+  			out << '<link href="/packages/' + group.to_s + '-mhtml.css" media="all" rel="stylesheet" type="text/css" />'
+  			out << '<![endif]-->'
+  			out << '<script src="/packages/' + group.to_s + '.js?' + Grundlebox::Version + '" type="text/javascript"></script>'
+  	  end
+	  elsif group == :grundlebox_show_article
+	    if defined?( Grundlebox::Application )
+	      out << include_stylesheets(:grundlebox_show_article, :media => "all")
+  		  out << include_stylesheets(:grundlebox_print_article, :media => "print")
+		  else
+        out << '<!--[if (!IE)|(gte IE 8)]><!-->'
+        out << '<link href="/packages/grundlebox_show_article-datauri.css?' + Grundlebox::Version + '" media="all" rel="stylesheet" type="text/css" />'
+        out << '<link href="/packages/grundlebox_print_article-datauri.css?' + Grundlebox::Version + '" media="print" rel="stylesheet" type="text/css" />'
+        out << '<!--<![endif]-->'
+        out << '<!--[if lte IE 7]>'
+        out << '<link href="/packages/grundlebox_show_article-mhtml.css" media="all" rel="stylesheet" type="text/css" />'
+        out << '<link href="/packages/grundlebox_print_article-mhtml.css" media="print" rel="stylesheet" type="text/css" />'
+        out << '<![endif]-->'
+      end
+    end
+    
+	  return out.html_safe
+	  
+  end
+  
 end
